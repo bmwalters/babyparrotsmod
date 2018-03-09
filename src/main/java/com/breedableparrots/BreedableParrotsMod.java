5249202e9eefc582ab35f27be9c302a4413e9fa0
Bradley Walters <zerfgog202@gmail.com>
2018-03-08 20:09:41 -0700

    Bump version to 1.0 and rename jar

--- src/main/java/com/breedableparrots/BreedableParrotsMod.java
+++ src/main/java/com/breedableparrots/BreedableParrotsMod.java
@@ -13,7 +13,7 @@ public class BreedableParrotsMod
 {
     public static final String MODID = "breedableparrotsmod";
     public static final String NAME = "Breedable Parrots Mod";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "1.0.0";
 
     public static Logger logger;
 
